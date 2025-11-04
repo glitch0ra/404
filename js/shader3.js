5bcca21ffab1b0a53bba2b3579259aac8c3b7ca1
glitch0ra <240775875+glitch0ra@users.noreply.github.com>
2025-11-05 02:53:33 +0300

    Update shader3.js

--- js/shader3.js
+++ js/shader3.js
@@ -228,7 +228,13 @@ document.addEventListener("DOMContentLoaded", () => {
   void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 uv = (fragCoord.xy * 2.0 - iResolution.xy) / iResolution.y;
     uv.x += 50.0 / iResolution.y; // ← сдвиг влево
-    float time = mod(iTime, 240.0) * SPEED;
+    
+    const float CYCLE = 10.0;
+    const float FADE_DURATION = 1.5;
+    float t = mod(iTime, CYCLE);
+    float alpha = smoothstep(CYCLE - FADE_DURATION, CYCLE, t);
+    float time = (t - CYCLE * alpha) * SPEED;
+    
     vec3 ro = vec3(0.5, 0.5, 0.0);
     vec3 rd = vec3(uv.x, 2.0, uv.y);
     vec3 col = rain(ro, rd, time);
@@ -333,3 +339,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
   requestAnimationFrame(render);
 });
+
