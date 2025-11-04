1a6f2f757e7134a9ca3e14492ecee6e95a51ebab
glitch0ra <240775875+glitch0ra@users.noreply.github.com>
2025-11-04 04:32:50 +0300

    Update shader3.js

--- js/shader3.js
+++ js/shader3.js
@@ -216,9 +216,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    // Сдвиг на 50px ВЛЕВО, чтобы убрать изначальное смещение вправо
-    vec2 adjustedFragCoord = fragCoord.xy - vec2(50.0, 0.0);
-    vec2 uv = (adjustedFragCoord * 2.0 - iResolution.xy) / iResolution.y;
+    vec2 uv = (fragCoord.xy * 2.0 - iResolution.xy) / iResolution.y;
+    uv.x += 50.0 / iResolution.y; // ← сдвиг влево на 50px
     float time = iTime * SPEED;
     vec3 ro = vec3(0.5, 0.5, 0.0);
     vec3 rd = vec3(uv.x, 2.0, uv.y);
@@ -305,5 +304,6 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   requestAnimationFrame(render);
 });
+
 
 
