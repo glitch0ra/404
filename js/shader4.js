9a7afe7c7a1b04c23306b5f4470e3a5895ecc37a
glitch0ra <240775875+glitch0ra@users.noreply.github.com>
2025-11-07 01:27:28 +0300

    Update shader4.js

--- js/shader4.js
+++ js/shader4.js
@@ -196,11 +196,11 @@ void main() {
   vec3 col = effect(p);
 
   // Сохраняем размытость горизонта (чуть ниже центра)
-  float horizon = smoothstep(0.01, 0.35, q.y);
-  col = mix(col, vec3(col * 3.2), horizon);
+  float horizon = smoothstep(0.01, 0.30, q.y);
+  col = mix(col, vec3(col * 1.5), horizon);
 
   // ✅ Тот же принцип разделения, что в оригинальном shader4.js
-  float alpha = smoothstep(0.65, 0.25, q.y);
+  float alpha = smoothstep(0.60, 0.25, q.y);
 
   col = aces_approx(col);
   col = sRGB(col);
@@ -247,6 +247,7 @@ void main() {
   }
   requestAnimationFrame(render);
 });
+
 
 
 
