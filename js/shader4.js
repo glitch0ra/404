c2af85494f9796745fa2670b498c3eb0f05d55bc
glitch0ra <240775875+glitch0ra@users.noreply.github.com>
2025-11-07 01:32:02 +0300

    Update shader4.js

--- js/shader4.js
+++ js/shader4.js
@@ -200,7 +200,7 @@ void main() {
   col = mix(col, vec3(col * 1.5), horizon);
 
   // ✅ Тот же принцип разделения, что в оригинальном shader4.js
-  float alpha = smoothstep(0.60, 0.25, q.y);
+  float alpha = smoothstep(0.60, 0.35, q.y);
 
   col = aces_approx(col);
   col = sRGB(col);
@@ -247,6 +247,7 @@ void main() {
   }
   requestAnimationFrame(render);
 });
+
 
 
 
