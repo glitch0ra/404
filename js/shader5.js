03b73b116d1c823600a196530489d95cb8893a4e
glitch0ra <240775875+glitch0ra@users.noreply.github.com>
2025-11-11 00:52:14 +0300

    Update shader5.js

--- js/shader5.js
+++ js/shader5.js
@@ -203,7 +203,7 @@ document.addEventListener('DOMContentLoaded', () => {
         );
         
         // Наклон вперёд (к камере) и влево
-        float tiltX = radians(1.0);  // наклон к камере
+        float tiltX = radians(70.0);  // наклон к камере
         float tiltZ = radians(-180.0);   // лёгкий поворот влево 
         
         mat3 rotX = mat3(
@@ -351,6 +351,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   requestAnimationFrame(render);
 });
+
 
 
 
