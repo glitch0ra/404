9827279c27bf626ccb41a0621748119bc15d5dba
glitch0ra <240775875+glitch0ra@users.noreply.github.com>
2025-11-11 19:55:18 +0300

    Update shader5.js

--- js/shader5.js
+++ js/shader5.js
@@ -335,7 +335,7 @@ document.addEventListener('DOMContentLoaded', () => {
         gl.uniform3f(iResolutionLoc, canvas.width, canvas.height, 1.0);
         gl.uniform1f(iTimeLoc, t);
         
-        const blurValue = 0.3; // РЕГУЛИРУЙ ЗНАЧЕНИЕ ОТ 0.0 ДО 1.0
+        const blurValue = 1.0; // РЕГУЛИРУЙ ЗНАЧЕНИЕ ОТ 0.0 ДО 1.0
         gl.uniform1f(ringBlurLoc, blurValue);
         
         gl.clearColor(0, 0, 0, 0);
@@ -346,3 +346,4 @@ document.addEventListener('DOMContentLoaded', () => {
     
     requestAnimationFrame(render);
 });
+
